aae7d5e053226068fe392bac5327166ab480b8d1
Andres Gaibor <androymartin99@gmail.com>
2024-01-30 12:07:58 -0500

    Controlador

---
+++ controller/Controller.php
@@ -0,0 +1,18 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+    $v1 = $_GET['var1'];
+
+    if($v1 == 1){
+        include('../View/Vingreso.php');
+    }
+    elseif($v1 == 2){
+        include('../View/VConsulta.php');
+    } elseif($v1 == 3) {
+        include('../View/VBuscar.php');
+    }
+    else "ninguna opcion seleccionada";
+
+?>
