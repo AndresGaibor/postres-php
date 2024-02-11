1e38920ad9fd1e4b59c33aee7d9e0c4b27568a79
svortus <jhosgonza24@gmail.com>
2024-02-11 12:33:18 -0500

    visualizar inconpleto
    visualizar sin probar

---
+++ Model/V_Visualizar_P_Usuario.php
@@ -0,0 +1,25 @@
+<?php
+    include("../config/config.php");
+    $sql="SELECT
+    p.nombre_producto,
+    p.id,
+    p.precio,
+    p.img_url,
+    GROUP_CONCAT(i.nombre_ingrediente SEPARATOR ', ') AS ingredientes
+    FROM tienda_postres.Producto p
+    INNER JOIN tienda_postres.ProductoIngrediente pi ON p.id = pi.producto_id
+    INNER JOIN tienda_postres.Ingrediente i ON pi.ingrediente_id = i.id
+    GROUP BY p.id;"
+    $resul = mysqli_query($conexion,$sql);
+    while($mostrar=mysqli_fetch_array($resul)){
+        ?>
+        <div class="card">
+            <img src="<?php echo $mostrar['img_url']?>" alt="">
+            <label for=""><?php echo $mostrar['p.nombre_producto']?></label>
+            <label for=""><?php echo $mostrar['p.id']?></label>
+            <label for=""><?php echo $mostrar['p.precio']?></label>
+            <label for=""><?php echo $mostrar['ingredientes']?></label>
+        </div>
+        <?php
+    }
+?>
