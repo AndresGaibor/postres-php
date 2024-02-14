01ff53951844adfdd8472a53a352f854fb009095
svortus <jhosgonza24@gmail.com>
2024-02-14 01:50:04 -0500

    8
    x

--- Model/M_Ingresar_Admin.php
+++ Model/M_Ingresar_Admin.php
@@ -7,9 +7,9 @@ $img=$_POST['imagen'];
 $idCat=$_POST['categoria'];
 $sql = "INSERT INTO producto (nombre_producto, precio, stock, img_url) 
         VALUES ('$nombre', $precio, $stock, '$img');
-        SET @producto_id = SELECT id FROM producto WHERE nombre_producto ='$nombre';
+        
         INSERT INTO ProductoCategoria (producto_id, categoria_id) 
-        VALUES (@producto_id, (SELECT id FROM Categoria WHERE id = $idCat));";
+        VALUES ((SELECT id FROM producto WHERE nombre_producto ='$nombre'), (SELECT id FROM Categoria WHERE id = $idCat));";
 mysqli_query($conexion,$sql);
 header('Location: ../index.php?pagina=productos');
 ?>
