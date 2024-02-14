322e3055c48e628c8d001c9d01f0b8481a479dd6
svortus <jhosgonza24@gmail.com>
2024-02-14 01:29:56 -0500

    correcion 4
    x

--- Model/M_Ingresar_Admin.php
+++ Model/M_Ingresar_Admin.php
@@ -5,9 +5,10 @@ $precio=$_POST['precio'];
 $stock=$_POST['cantidad'];
 $img=$_POST['imagen'];
 $idCat=$_POST['categoria'];
-$sql="INSERT INTO producto(nombre_producto,precio,stock,img_url)VALUES('$nombre',$precio,$stock,'$img');
-INSERT INTO ProductoCategoria(producto_id, categoria_id) 
-VALUES((SELECT LAST_INSERT_ID()), (SELECT id FROM Categoria WHERE id = '$idCat'));";
+$sql = "INSERT INTO producto (nombre_producto, precio, stock, img_url) 
+        VALUES ('$nombre', $precio, $stock, '$img');
+        INSERT INTO ProductoCategoria (producto_id, categoria_id) 
+        VALUES (LAST_INSERT_ID(), (SELECT id FROM Categoria WHERE id = '$idCat'));";
 mysqli_query($conexion,$sql);
 header('Location: ../index.php?pagina=productos');
 ?>
