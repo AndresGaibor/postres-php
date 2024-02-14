d822ede1e5c9023490acd31e07d03d82909813d1
svortus <jhosgonza24@gmail.com>
2024-02-14 02:51:46 -0500

    ./

--- View/V_Btn_Eli_Edi.php
+++ View/V_Btn_Eli_Edi.php
@@ -1,3 +1,3 @@
 <div class="container">
-<button type="button" class="btn btn-primary" ><a href="./v_elimainar_producto.php">Eliminar/Editar</a></button>
+<button type="button" class="btn btn-primary" ><a href="./View/v_elimainar_producto.php">Eliminar/Editar</a></button>
 </div>
