915d5e7159ded99b0cdb33043d621b6cb7b01e83
30-mvaa <138589025+30-mvaa@users.noreply.github.com>
2024-02-13 18:33:42 -0800

    Add files via upload

---
+++ View/v_elimainar_producto.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Eliminar/editar</title>    
+  <link rel="stylesheet" href="../css/holaaa.css">
+  <!-- UIkit CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.16.22/dist/css/uikit.min.css" />
+
+</head>
+<body>
+    <center><h1> Mostrar datos del estudiante</h1> 
+    <table border="1" >
+    <th >Id</th>
+    <th >Nombre</th>
+    <th >Precio </th>
+    <th >Cantidad </th>
+    <th >Foto </th>
+    <th >Modificar  </th>
+    <th >Eliminar</th>
+    <?php include("../Model/EditarMostrar.php")?>
+  </tr>
+</table>
+    </center>
+</body>
+
+</html
