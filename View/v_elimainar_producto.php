7384d08f7b39e4897d0e7a92037b148eebeb7911
svortus <jhosgonza24@gmail.com>
2024-02-14 06:14:30 -0500

    s
    s

--- View/v_elimainar_producto.php
+++ View/v_elimainar_producto.php
@@ -1,21 +1,6 @@
 
-<!DOCTYPE html>
-<html lang="es">
+<div class="container mt-5">
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Eliminar/editar</title>    
-  <link rel="stylesheet" href="../css/holaaa.css">
-  <!-- UIkit CSS -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.16.22/dist/css/uikit.min.css" />
-
-  <!-- Agregar script de UIkit -->
-  <script src="https://cdn.jsdelivr.net/npm/uikit@3.16.22/dist/js/uikit.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/uikit@3.16.22/dist/js/uikit-icons.min.js"></script>
-
-</head>
-<body>
   <center><h1> Producto</h1> 
     <table border="1" >
       <tr>
@@ -31,10 +16,12 @@
     </table>
   </center>
 
-
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
  
 
-</body>
+</div>
 
-</html>
+
 
