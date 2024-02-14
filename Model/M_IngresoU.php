49158e73613ecaf8ddf263ebb00b51fd557b206e
Andres Gaibor <androymartin99@gmail.com>
2024-02-14 03:38:15 -0500

    Añadir validación de provincia y ciudad en el formulario de registro

--- Model/M_IngresoU.php
+++ Model/M_IngresoU.php
@@ -1,13 +1,19 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include('../config/config.php');
+
 // Obtener valores del formulario
 $nombre = $_POST['nombre'];
 $apellido = $_POST['apellido'];
 $clave = sha1($_POST['clave']);
 $email = $_POST['email'];
 $telefono = $_POST['telefono'];
-$provincia = $_POST['provincia'];
 $ciudad = $_POST['ciudad'];
 $calle = $_POST['calle'];
+$calle2 = $_POST['calle2'];
 
 // Consulta SQL para verificar si el correo ya está en uso
 $sql_verificar = "SELECT * FROM usuario WHERE correo = '$email'";
@@ -21,22 +27,12 @@ if(mysqli_num_rows($resultado_verificar) > 0) {
     // Si el correo no está en uso, proceder con la inserción
 
     // Insertar provincia y obtener su ID
-    $sql_insert_provincia = "INSERT INTO provincia (nombre_provincia) VALUES ('$provincia')";
-    mysqli_query($conexion, $sql_insert_provincia);
-    $id_provincia = mysqli_insert_id($conexion);
-
-    // Insertar ciudad y obtener su ID
-    $sql_insert_ciudad = "INSERT INTO ciudad (nombre_ciudad, provincia_id) VALUES ('$ciudad', '$id_provincia')";
-    mysqli_query($conexion, $sql_insert_ciudad);
-    $id_ciudad = mysqli_insert_id($conexion);
-
-    // Insertar calle y obtener su ID
-    $sql_insert_calle = "INSERT INTO direccion (calle_principal, ciudad_id) VALUES ('$calle', '$id_ciudad')";
+    $sql_insert_calle = "INSERT INTO direccion (calle_principal, calle_secundaria, ciudad_id) VALUES ('$calle', '$calle2', '$ciudad')";
     mysqli_query($conexion, $sql_insert_calle);
     $id_direccion = mysqli_insert_id($conexion);
 
     // Insertar usuario con el ID de dirección obtenido
-    $sql_insert_usuario = "INSERT INTO usuario (nombre, apellido, direccion_id, correo, clave, rol, telefono) VALUES ('$nombre', '$apellido', '$id_direccion', '$email', '$clave', '', '$telefono')";
+    $sql_insert_usuario = "INSERT INTO usuario (nombre, apellido, direccion_id, correo, clave, rol, telefono) VALUES ('$nombre', '$apellido', '$id_direccion', '$email', '$clave', null, '$telefono')";
     $ejecutar = mysqli_query($conexion, $sql_insert_usuario);
     
     if($ejecutar) {
--- View/V_CrearCuenta.php
+++ View/V_CrearCuenta.php
@@ -1,42 +1,78 @@
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <div class="container-deregistro mx-auto">
-        <h2>Registro de Usuario</h2>
-        <form action="./Model/M_IngresoU.php" method="post">
-            <label for="nombre">Nombre*</label>
-            <input type="text" id="nombre" name="nombre" required>
-            <br>
-            <label for="apellido">Apellido*</label>
-            <input type="text" id="apellido" name="apellido" required>
-            <br>
-            <label for="Provincia">Provincia*</label>
-            <select id="provincia" name="provincia">
-                <option value="Escoja una">Escoja una</option>
-                <!-- Otras opciones de provincias -->
-            </select>
-            <br>
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-            <label for="Ciudad">Ciudad*</label>
-            <select id="ciudad" name="ciudad">
-                <option value="Escoja una">Escoja una</option>
-            </select>
-            <br>
+$provincia = isset($_GET['provincia']) ? $_GET['provincia'] : '';
 
-            <label for="Calle">Calle Principal*</label>
-            <select id="calle" name="calle">
-                <option value="Escoja una">Escoja una</option>
-            </select>
+include('./config/config.php');
 
-            <br>
-            <label for="email">Email*</label>
-            <input type="email" id="email" name="email" required>
-            <br>
-            <label for="clave">Clave*</label>
-            <input type="password" id="clave" name="clave" required>
-            <br>
-            <label for="telefono">Teléfono*</label>
-            <input type="tel" id="telefono" name="telefono" required>
-            <br>
-            <input type="submit" name="enviar" value="Registrar">
 
-        </form>
-    </div>
+$sql = "SELECT * FROM provincia";
+$resultado = mysqli_query($conexion, $sql);
+
+if ($provincia != '') {
+    $sql2 = "SELECT * FROM Ciudad WHERE provincia_id = $provincia";
+    $resultado2 = mysqli_query($conexion, $sql2);
+} else {
+    echo 'No hay provincia seleccionada';
+}
+?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<div class="container-deregistro mx-auto">
+    <h2>Registro de Usuario</h2>
+    <form action="./Model/M_IngresoU.php" method="post">
+        <label for="nombre">Nombre*</label>
+        <input type="text" id="nombre" name="nombre" required>
+        <br>
+        <label for="apellido">Apellido*</label>
+        <input type="text" id="apellido" name="apellido" required>
+        <br>
+        <label for="Provincia">Provincia*</label>
+        <select id="provincia" name="provincia"
+         required>
+            <option value="">Escoja una</option>
+            <?php while ($fila = mysqli_fetch_array($resultado)) { ?>
+                <option <?php echo $fila['id'] == $provincia ? 'selected' : '' 
+                     ?> value="<?php echo $fila['id']; ?>"><?php echo $fila['nombre_provincia']; ?></option>
+            <?php } ?>
+            <!-- Otras opciones de provincias -->
+        </select>
+        <br>
+
+        <label for="Ciudad">Ciudad*</label>
+        <select id="ciudad" name="ciudad" required <?php echo $provincia == '' ? 'disabled' : '';?> >
+            <option value="">Escoja una</option>
+            <?php if($provincia != '') { while ($fila2 = mysqli_fetch_array($resultado2)) { ?>
+                <option value="<?php echo $fila2['id']; ?>"><?php echo $fila2['nombre_ciudad']; ?></option>
+            <?php }} ?>
+        </select>
+        <br>
+
+        <label for="Calle">Calle Principal*</label>
+        <input id="calle" type="text" name="calle" required />
+        <label for="Calle">Calle Secundaria</label>
+        <input id="calle" type="text" name="calle2" />
+
+        <br>
+        <label for="email">Email*</label>
+        <input type="email" id="email" name="email" required>
+        <br>
+        <label for="clave">Clave*</label>
+        <input type="password" id="clave" name="clave" required>
+        <br>
+        <label for="telefono">Teléfono*</label>
+        <input type="tel" id="telefono" name="telefono" required>
+        <br>
+        <input type="submit" name="enviar" value="Registrar">
+
+    </form>
+</div>
+<script>
+document.getElementById('provincia').addEventListener('change', async function() {
+    var provincia = this.value;
+    
+    // fetch(`./?pagina=crearcuenta&provincia=${provincia}`)
+    location.href = `./?pagina=crearcuenta&provincia=${provincia}`;
+});
+</script>
